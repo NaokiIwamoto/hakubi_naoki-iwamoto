7713f43e2576d48f926d6fe3b8f1510fb314cf40
iwamoto <noichi1132k@gmail.com>
2019-06-19 20:43:54 +0800

    Implement learned

---
+++ resources/views/user/learned_words.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/text_arrange.css') }}">
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <table class="table table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Japanese</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($learned_words as $word)
+                <tr>
+                    <th scope="row">{{ $word->learnedwordGetWord()->first()->japanese }}</th>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
