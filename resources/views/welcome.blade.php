99c19af9754b5b8170ded243798ca2ac0577bb72
NaokiIwamoto <49220824+NaokiIwamoto@users.noreply.github.com>
2019-06-20 09:30:44 +0800

    Modified welcomepage (#11)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,12 @@
 @endsection
 
 @section('content')
-<div id="welcom_background">
+<div id="welcom_background" class="d-flex align-items-center">
     <div class="row">
         <div class="col-sm-9"></div>
-        <div class="col-sm-3 text-center">
+        <div class="col-sm-3 text-center tokyo-text">
             <p class="text-white">
-                Tokyo was selected as the host city during the 125th IOC Session in Buenos Aires on 7 September 2013.[3] These Games will mark the return of the Summer Olympics to Tokyo for the first time since 1964, the first city in Asia to host the Olympics twice, and the fourth Olympics overall to be held in Japan, following the 1972 Winter Olympics in Sapporo and the 1998 Winter Olympics in Nagano. They will be the second of three consecutive Olympic Games to be held in East Asia, following the 2018 Winter Olympics in Pyeongchang, South Korea, and preceding the 2022 Winter Olympics in Beijing, China.
+                Tokyo was selected as the host city during the 125th IOC Session in Buenos Aires on 7 September 2013. These Games will mark the return of the Summer Olympics to Tokyo for the first time since 1964, the first city in Asia to host the Olympics twice, and the fourth Olympics overall to be held in Japan, following the 1972 Winter Olympics in Sapporo and the 1998 Winter Olympics in Nagano. They will be the second of three consecutive Olympic Games to be held in East Asia, following the 2018 Winter Olympics in Pyeongchang, South Korea, and preceding the 2022 Winter Olympics in Beijing, China.
             </p>
         </div>
     </div>
