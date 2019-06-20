e5e1bddec52424fe2147d4d30c2f9e012ce1a6c8
iwamoto <noichi1132k@gmail.com>
2019-06-20 11:22:08 +0800

    test1

--- resources/views/user/edit_icon.blade.php
+++ resources/views/user/edit_icon.blade.php
@@ -2,6 +2,7 @@
 
 @section('css')
 <link href="/css/home.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 @endsection
 
 @section('content')
